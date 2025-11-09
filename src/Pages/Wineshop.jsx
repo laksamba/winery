ab8196f86ba66aa3cf64318f2f5c62dd2b8ec4ea
aarjudahal <aarjudahal2061@gmail.com>
2025-11-09 15:21:33 +0545

    fixed line

--- src/Pages/Wineshop.jsx
+++ src/Pages/Wineshop.jsx
@@ -10,8 +10,11 @@ export default function WineShop() {
       </h2>
 
       <h3 className="text-4xl font-bold text-[#4c1d29] -mt-14 mb-14 relative">
-        <span className="bg-transparent px-6 relative z-10 font-[cursive]">Our Wines</span>
-        <span className="absolute left-1/2 top-1/2 w-[200px] h-[1px] bg-[#4c1d29] -translate-x-1/2"></span>
+        <div className="flex justify-center items-center">
+            <hr  className="border-2 w-14 border-[#4c1d29]/50"/>
+            <span className="bg-transparent px-6 relative z-10 font-[cursive]">Our Wines</span>
+            <hr  className="border-2 w-14 border-[#4c1d29]/50"/>
+        </div>
       </h3>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto px-6">
