5f8cd1158d04af12114189a8b6ce888453fbae94
aarjudahal <aarjudahal2061@gmail.com>
2025-11-09 14:59:03 +0545

    wine

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Home } from '../Pages/Home';
+import WineShop from '../Pages/Wineshop';
 
 const Layout = () => {
   return (
     <div>
       <Home/>
+      <WineShop/>
     </div>
   )
 }
